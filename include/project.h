3f009db874b52b3ab511b870544322ac90eb6036
patrick <patrickkevinmims@gmail.com>
2023-06-10 16:23:44 -0700

    latest

--- include/project.h
+++ include/project.h
@@ -3,8 +3,9 @@
 
 // Assume that each word is no more than 20 characters long
 #define INDEX 20
+#define SIZE 3
 
 void store(char *);
-void read(char[], int, int);
+void read(char[], int);
 
 #endif
