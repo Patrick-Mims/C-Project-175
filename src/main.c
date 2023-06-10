d31cfff174d16a14e20eefd0c8ec703b6e56011a
patrick <patrickkevinmims@gmail.com>
2023-06-10 15:47:10 -0700

    added a static in variable to keep track of the number of items in the array

--- src/main.c
+++ src/main.c
@@ -9,11 +9,14 @@ void store(char *word)
     printf("I will attempt to store this word => %s\n", word); 
 }
 
-char *read(char str[], int n)
+void read(char str[], int n, int c)
 {
+    char *storage[3];
     char *strPtr = NULL;
     int character;
-    int i = 0;
+    int i = 0, j;
+    static int counter = 0;
+
 
     // get the string
     while((character = getchar()) != '\n')
@@ -26,24 +29,35 @@ char *read(char str[], int n)
 
     str[i] = '\0';
 
-    // malloc the string
-    if((strPtr = malloc(sizeof(strlen(str) + 1))) == NULL)
+    if((storage[c] = malloc(sizeof(char) * INDEX)) == NULL)
     {
         printf("Cannot Allocate Memory\n");
         exit("EXIT_FAILURE");
     }
 
-    strcpy(strPtr, str);
+    strcpy(storage[counter], str);
+    counter++;
 
-    store(strPtr);
+    if(counter == 3)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("storage => %s\n", storage[j]);
+        }
+    }
 
-    printf("strPtr => %s\n", strPtr);
+/*
+    */
+
+    //storage[c] = &str;
+
+    //strcpy(strPtr, str);
+    // store(strPtr);
+    //printf("strPtr => %s\n", strPtr);
 
     free(strPtr);
-
     // return the length of the string
     // return strlen(str);
-    return str;
 }
 
 int main(int argc, char **argv)
@@ -52,7 +66,6 @@ int main(int argc, char **argv)
     char *message[INDEX];
     const int SIZE = 3;
     char *ptr[SIZE];
-    char *result = NULL;
 
     printf("Welcome\n");
 
@@ -60,19 +73,18 @@ int main(int argc, char **argv)
     {
         printf("Enter word: ");
 
-        //len = read(message, INDEX);
-        //ptr[count] = read(message, INDEX);
-        ptr[count] = read(message, INDEX);
-
-        //printf("[word lenth]: %d - %s\n", len, *ptr[count]);
+        read(message, INDEX, count);
 
         count++;
     } while(count < SIZE);
 
+/*
+    // Print the Array
     for(int i = 0; i < SIZE; i++)
     {
-        printf("=> %s\n", ptr[i]);
+        printf("=> %s\n", message[i]);
     }
+    */
 
     return 0;
 }
